3681fe9016ffdeb1396bbdc7f29548d84f0d3c33
fnm04-sh <91214755+fnm04-sh@users.noreply.github.com>
2024-11-16 03:46:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Отзывы плавно появляются при прокрутке
     const reviews = document.querySelectorAll('.review-box');
     reviews.forEach((review, index) => {
         setTimeout(() => {
             review.style.opacity = '1';
             review.style.transform = 'translateY(0)';
-        }, index * 500);  // Задержка для плавного появления
+        }, index * 300);
     });
 });
